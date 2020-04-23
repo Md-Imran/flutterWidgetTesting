e31e769699ce272eb0d63e17968b73418c59ffae
Md-Imran <imranku11@gmail.com>
2020-04-23 21:45:38 +0600

    operation of StatefulWidget

--- lib/StatefullClasssTest.dart
+++ lib/StatefullClasssTest.dart
@@ -15,12 +15,42 @@ class MyApp extends StatefulWidget{
 
 }
 class MyAppState extends State<MyApp>{
+var questionVariable=0;
+  void answerQuestion() {
+    setState(() {
+      questionVariable=questionVariable+1;
+    });
+    print(questionVariable);
+  }
 
   @override
   Widget build(BuildContext context) {
+    var question = [
+      'what\'s your first questio,',
+      'what\'s your second question'
+    ];
     // TODO: implement build
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Test Title'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(question.elementAt(0)),
+              RaisedButton(
+                  child: Text('Question 1 : What\'s your name buddy'),
+                  onPressed: answerQuestion),
+              RaisedButton(
+                child: Text('Question 2 : Whats\; your age '),
+                onPressed: () => print('Answer 2 chossen'),
+              ),
+              RaisedButton(
+                child: Text('Question 3 : There is no question '),
+                onPressed: (answerQuestion) ,
+              )
+            ],
+          )),
+    );
   }
-
-
 }
